9cefd8f88de7fea5ab36197b477ffb2cf38d75d1
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-10-05 17:36:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
 
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", "20211998"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "beeeebe74c0c467c47c6ac4a1c9d75b5")
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6626958529:AAEPm42AevBDyBZQ5sM9i1zpElktUBwHGKM")
 
     CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME")
